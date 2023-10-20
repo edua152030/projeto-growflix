d4969456d71bf9808e31df2c01e30b995b0e668e
edua152030 <eduardo.vaz.faria@gmail.com>
2023-10-20 11:12:30 -0300

    inicial

---
+++ src/assets/js/data.js
@@ -0,0 +1,9 @@
+const videos = [
+
+    {
+        imagen: 'https://img.youtube.com/vi/aJ-HZuLcKeA/sddefault.jpg',
+        titulo: 'growcast #00',
+        link: 'o embed so src',
+        categoria: 'Growcast'
+    }
+]
